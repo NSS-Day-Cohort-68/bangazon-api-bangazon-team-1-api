7a0e5227d2db1290045bd18cffb17c3146272ff8
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-05 09:16:35 -0500

    🪲💥🔫

--- bangazonapi/models/orderproduct.py
+++ bangazonapi/models/orderproduct.py
@@ -5,7 +5,7 @@ class OrderProduct(models.Model):
 
     order = models.ForeignKey("Order",
                               on_delete=models.DO_NOTHING,
-                              related_name="order_products")
+                              related_name="lineitems")
 
     product = models.ForeignKey("Product",
                                 on_delete=models.DO_NOTHING,
