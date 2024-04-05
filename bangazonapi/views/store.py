7a5e0a55763ae34344c70cc5864aefd3c104626d
Emily Mai <emily.vy.mai@gmail.com>
2024-04-05 14:59:07 -0500

    store module

--- bangazonapi/views/store.py
+++ bangazonapi/views/store.py
@@ -28,13 +28,13 @@ class Stores(ViewSet):
         @apiHeaderExample {String} Authorization
             Token 9ba45f09651c5b0c404f37a2d2572c026c146611
 
-        @apiParam {String} store_name Name of the store.
-        @apiParam {String} store_desc Description of the store.
+        @apiParam {String} name Name of the store.
+        @apiParam {String} description Description of the store.
 
         @apiSuccess {Number} id ID of the newly created store.
         @apiSuccess {String} customer_id Customer ID associated with the store.
-        @apiSuccess {String} store_name Name of the store.
-        @apiSuccess {String} store_desc Description of the store.
+        @apiSuccess {String} name Name of the store.
+        @apiSuccess {String} description Description of the store.
 
         @apiSuccessExample Success
             {
@@ -57,8 +57,39 @@ class Stores(ViewSet):
             serializer = StoreSerializer(new_store, context={"request": request})
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
         except KeyError:
             # Handle missing name or description in request data
-            return Response({"error": "Missing name or description in request data"}, status=status.HTTP_400_BAD_REQUEST)
-        
+            return Response(
+                {"error": "Missing name or description in request data"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    def retrieve(self, request, pk=None):
+        """
+        @api {GET} /stores/:id GET stores
+        @apiName GetStore
+        @apiGroup Store
+
+        @apiParam {id} id Store Id
+
+        @apiSuccess (200) {Object} store Created store
+        @apiSuccess (200) {id} store.id Store id
+        @apiSuccess (200) {int} store.owner_id
+        @apiSuccess (200) {String} store.name
+        @apiSuccess (200) {String} store.description
+
+        @apiSuccessExample Success
+            {
+                "id": 1,
+                "customer_id": 5,
+                "name": "Tech Emporium",
+                "description": "Your one-stop shop for all things tech! Find the latest gadgets, accessories, and more."
+            }
+        """
+        try:
+            store = Store.objects.get(pk=pk)
+            serializer = StoreSerializer(store, context={"request": request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
