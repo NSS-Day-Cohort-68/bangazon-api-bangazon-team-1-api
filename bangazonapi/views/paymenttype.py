28ba9d18fa5ae2915ae85982344098d4cc319bba
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-12 09:38:12 -0500

    added obscured_num property to payment type

--- bangazonapi/views/paymenttype.py
+++ bangazonapi/views/paymenttype.py
@@ -1,4 +1,5 @@
 """View module for handling requests about customer payment types"""
+
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
@@ -13,14 +14,27 @@ class PaymentSerializer(serializers.HyperlinkedModelSerializer):
     Arguments:
         serializers
     """
+
+    obscured_num = serializers.SerializerMethodField()
+
     class Meta:
         model = Payment
         url = serializers.HyperlinkedIdentityField(
-            view_name='payment',
-            lookup_field='id'
+            view_name="payment", lookup_field="id"
         )
-        fields = ('id', 'url', 'merchant_name', 'account_number',
-                  'expiration_date', 'create_date')
+        fields = (
+            "id",
+            "url",
+            "merchant_name",
+            "account_number",
+            "expiration_date",
+            "create_date",
+            "obscured_num",
+        )
+
+    def get_obscured_num(self, obj):
+        account_num = obj.account_number
+        return f"{'*' * (len(account_num) - 4)}{account_num[-4:]}"
 
 
 class Payments(ViewSet):
@@ -40,8 +54,7 @@ class Payments(ViewSet):
         new_payment.customer = customer
         new_payment.save()
 
-        serializer = PaymentSerializer(
-            new_payment, context={'request': request})
+        serializer = PaymentSerializer(new_payment, context={"request": request})
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -53,8 +66,7 @@ class Payments(ViewSet):
         """
         try:
             payment_type = Payment.objects.get(pk=pk)
-            serializer = PaymentSerializer(
-                payment_type, context={'request': request})
+            serializer = PaymentSerializer(payment_type, context={"request": request})
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
@@ -72,15 +84,18 @@ class Payments(ViewSet):
             return Response({}, status=status.HTTP_204_NO_CONTENT)
 
         except Payment.DoesNotExist as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"message": ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     def list(self, request):
         """Handle GET requests to payment type resource"""
         customer = Customer.objects.get(user=request.auth.user)
         payment_types = Payment.objects.filter(customer=customer)
         serializer = PaymentSerializer(
-            payment_types, many=True, context={'request': request})
+            payment_types, many=True, context={"request": request}
+        )
         return Response(serializer.data)
