9c657f1ed740c7e71f69956442c887fea341a181
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-12 10:07:53 -0500

    added "merchant" and "acctNumber" field checks
    to align with client-side expectations

--- bangazonapi/views/paymenttype.py
+++ bangazonapi/views/paymenttype.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from bangazonapi.models import Payment, Customer
+from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
 
 class PaymentSerializer(serializers.HyperlinkedModelSerializer):
@@ -45,18 +48,47 @@ class Payments(ViewSet):
         Returns:
             Response -- JSON serialized payment instance
         """
-        new_payment = Payment()
-        new_payment.merchant_name = request.data["merchant_name"]
-        new_payment.account_number = request.data["account_number"]
-        new_payment.expiration_date = request.data["expiration_date"]
-        new_payment.create_date = request.data["create_date"]
-        customer = Customer.objects.get(user=request.auth.user)
-        new_payment.customer = customer
-        new_payment.save()
 
-        serializer = PaymentSerializer(new_payment, context={"request": request})
+        """
+        {
+            "acctNumber": "1234123412341234",
+            "merchant": "Bruh"
+        }
+        """
+        try:
+            new_payment = Payment()
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            merchant_name = request.data.get("merchant_name")
+            if not merchant_name:
+                merchant_name = request.data.get("merchant")
+
+            account_number = request.data.get("account_number")
+            if not account_number:
+                account_number = request.data.get("acctNumber")
+
+            customer = Customer.objects.get(user=request.auth.user)
+
+            new_payment.merchant_name = merchant_name
+            new_payment.account_number = account_number
+            new_payment.expiration_date = request.data.get(
+                "expiration_date", "0000-00-00"
+            )
+            new_payment.create_date = request.data.get(
+                "create_date", datetime.now().date()
+            )
+            new_payment.customer = customer
+            new_payment.save()
+
+            serializer = PaymentSerializer(new_payment, context={"request": request})
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except IntegrityError as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+        except ValidationError:
+            return Response(
+                {"message": "Payment must include a valid expiration date"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single payment type
